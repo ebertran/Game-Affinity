f260b517d63e99347338bbdcb015595e07295931
Enric Bertran Martí <enric@MacBook-Pro-de-Enric.local>
2017-10-17 16:09:16 +0200

    items add to modify

---
+++ testLogic.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+class Test extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            testResult: []
+        }
+    }
+
+
