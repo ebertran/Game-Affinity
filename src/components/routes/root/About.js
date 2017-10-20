92f86ca08b1bcfe5f15065038bed4283cc1191db
Enric Bertran Martí <enric@MacBook-Pro-de-Enric.local>
2017-10-20 14:10:06 +0200

    final commit

--- src/components/routes/root/About.js
+++ src/components/routes/root/About.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Jumbotron, Button, Grid, Row, Col, Image } from 'react-bootstrap'
 
 const About = () => (
-  <h1>This is About page</h1>
+  <Grid>
+    <h2>About the test</h2>
+    <p>The Bartle taxonomy of player types is a classification of video game players based on a 1996 paper by Richard Bartle according 
+    to their preferred actions within the game. 
+    The classification originally described players of multiplayer online games, 
+    though now it also refers to players of single-player video games.</p>
+    <h2>About us</h2>
+    <p>We are three developers who likes to make new interesting things, like this one.
+    The project is maded in a week, approximately, so... there are many things to improve.
+    We hope that you enjoy our app.</p>
+  </Grid>
 )
 
 export default About
